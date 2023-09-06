d7f1afeec5da73bd2c9c893be6428e24840088b8
Léo Oliveira <leo.oliveira2015@outlook.com>
2023-09-05 21:45:15 -0300

    gerenciador-de-usuario-2

--- index.js
+++ index.js
@@ -1,25 +1,41 @@
-var fields = document.querySelectorAll("#form-user-create [name]")
-fields.forEach(function (field, index) {
+var fields = document.querySelectorAll("#form-user-create [name]");
 
-    if (field.name == "gender") {
+var user = {};
 
-        if (field.checked) {console.log("sim", field)}
+function addLine(dataUser) {
+  var tr = document.createElement("tr");
 
+  tr.innerHTML = `
+    <tr>
+      <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+      <td>${dataUser.name}</td>
+      <td>${dataUser.email}</td>
+      <td>${dataUser.admin}</td>
+      <td>${dataUser.birth}</td>
+      <td>
+        <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+        <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+      </td>
+    </tr>
+    `;
 
-    } else {
-        console.log("não")
-    }
-
-    // console.log(field.id, field.name, field.value, index)
-
-})
-
-var data = {
-
-    name: "hcode",
-    email: "hcode@hcode.com.br",
-    year: 2023
-
+    document.getElementById("table-users").appendChild(tr)
 }
 
-console.log(typeof data.name)
+document
+  .getElementById("form-user-create")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    fields.forEach(function (field, index) {
+      if (field.name == "gender") {
+        if (field.checked) {
+          user[field.name] = field.value;
+        }
+      } else {
+        user[field.name] = field.value;
+      }
+    });
+
+    addLine(user);
+  });
